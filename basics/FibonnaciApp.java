98fa77dd0c89830aabd3ac213130969e8a913f6d
ZappBrannigann <36939236+ZappBrannigann@users.noreply.github.com>
2019-06-03 16:10:24 -0700

    Add files via upload

---
+++ basics/FibonnaciApp.java
@@ -0,0 +1,36 @@
+package basics;
+
+public class FibonnaciApp {
+
+	public static void main(String[] args) {
+		// Fibonnaci is defined by the sum of the two previous Fibonnaci numbers.
+		// fib(0) = 0
+		// fib(1) = 1
+		// fib(2) = fib(1) + fib(0) = 0 + 1 = 1
+		// fib(3) = fib(2) + fib(1) = 1 + 1 = 2
+		// fib(4) = fib(3) + fib(2) = 2 + 1 = 3
+		// fib(5) = fib(4) + fib(3) = 3 + 2 = 5
+		System.out.println(fib(5));
+		
+		// Factorial, denoted by n!, is the product of all positive integers less than or equal to n.
+		// For example, the value of 0! is 1. 
+		// 1! = 1 * 1
+		// 2! = 2 * 1
+		// 3! = 3 * 2 * 1
+		// 4! = 4 * 3 * 2 * 1
+		
+	}
+	
+	
+	// Recursion 
+	public static int fib(int n) {
+		if (n == 0) {
+			return 0;
+		}
+		else if (n == 1) {
+			return 1;
+		}
+		return (fib(n-1) + fib(n-2));
+	}
+
+}
